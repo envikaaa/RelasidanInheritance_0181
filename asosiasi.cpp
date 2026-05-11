4244c131d11633b9a69a7cf99d59c6df9ec85683
envikaaa <envikapriyatna31@gmail.com>
2026-05-11 15:41:43 +0700

    menambahkan cout

--- asosiasi.cpp
+++ asosiasi.cpp
@@ -50,6 +50,6 @@ void dokter::tambahPasien(pasien*pPasien) {
 void dokter::cetakPasien() {
     cout << "Daftar pasien dari dokter\" << this->nama << "\":\n";
     for (auto& a : daftar_pasien) {
-        cout << 
+        cout << a->nama << "\n";
     }
 }
